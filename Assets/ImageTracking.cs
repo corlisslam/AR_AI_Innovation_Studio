d305121a7b9d0a5d760314206c097dd9b20d679f
CL Lam <corlisslam@gmail.com>
2024-07-06 01:00:49 +0100

    changed script, models/prefabs now instantiate at a fixed location

--- Assets/ImageTracking.cs
+++ Assets/ImageTracking.cs
@@ -1,134 +1,55 @@
-// This code is attached to the XR Orirgin game object because it is added as a component to it
-using System.Collections; 
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
-
-public class ARImageTracker : MonoBehaviour // Defining new class inheriting from MonoBehav, which is smthing for Unity
+public class SetUpModels : MonoBehaviour
 {
-    private ARTrackedImageManager _trackedImageManager; // Define instance of the class ARTrackedImageManager
-    //private ARAnchorManager _anchorManager;
+    [SerializeField]
+    private GameObject[] prefabsToSpawn;
 
-    [SerializeField] // Allows you to modify the values directly in the editor, even while the game is running
-    private List<PrefabWithTransform> prefabsToInstantiate; // List of prefabs to instantiate
+    private ARTrackedImageManager trackedImageManager;
 
-    [SerializeField] // A field that references the AR camera in editor
-    private Camera arCamera; // Reference to the AR camera
+    private Dictionary<string, GameObject> instantiatedPrefabs = new Dictionary<string, GameObject>();
 
-    private void Awake() // This method is called when the script is loaded, private void just means private for methods
+    private void Awake()
     {
-        _trackedImageManager = GetComponent<ARTrackedImageManager>(); // Initialize _trackedImageManager to the component attached to game obejct, is it now an instance of the ARTrackedImageManager class
-        //_anchorManager = GetComponent<ARAnchorManager>();
-        if (arCamera == null) // If arCamera is not set, set it to the main camera in the scene
+        trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
-            arCamera = Camera.main; // Optionally find the main camera if not set
+            SpawnPrefabs(trackedImage);
         }
     }
 
-    private void OnEnable() // This method is called when the script is enabled
+    private void SpawnPrefabs(ARTrackedImage trackedImage)
     {
-        if (_trackedImageManager != null)
+        if (instantiatedPrefabs.ContainsKey(trackedImage.referenceImage.name))
         {
-            _trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged; // The += operator is used to subscribe to OnTrackedImagesChanged method to the trackedImagesChanged event, so that the method is called when the event is raised.
-        } // TrackedImagesChanged is an event of the ARTrackedImageManager class
-
-    }
-
-    private void OnDisable() // This method called when the script is disabled
-    {
-        if (_trackedImageManager != null)
-        {
-            _trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
-        }
-    }
-
-    // OnTrackedImagesChanged is an event handler method
-    // ARTrackedImagesChangedEventArgs is a class that contains information about the tracked images that have changed
-    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs) // Event handler called when tracked images change (added, updated, removed)
-    // eventArgs is an instance of ARTrackedImagesChangedEventArg
-    {
-
-        foreach (ARTrackedImage trackedImage in eventArgs.added) //Iterates through each newly added tracked image and updates the AR content
-        // ARTrackedImage is a list of objects that were added
-        {
-            InstantiatePrefabs();
-        }
- 
-        foreach (ARTrackedImage trackedImage in eventArgs.updated)
-        {
-            // No updates yet
+            return; // Prefabs already spawned for this image
         }
 
-        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        foreach (GameObject prefab in prefabsToSpawn)
         {
-            // Handle removed images if needed
-        }
-    }
+            //Vector3 position = trackedImage.transform.position;
+            //Quaternion rotation = trackedImage.transform.rotation;
 
-    private void InstantiatePrefabs() // This method updates the AR content for a given tracked image.
-    {
-        //if (worldAnchor == null) return;
-        List<GameObject> instantiatedObjects = new List<GameObject>();
-
-        GameObject anchorObject = new GameObject("AnchorObject");
-        anchorObject.transform.position = arCamera.transform.position;
-        anchorObject.transform.rotation = arCamera.transform.rotation;
-
-        ARAnchor anchor = anchorObject.AddComponent<ARAnchor>();
-
-
-        foreach (PrefabWithTransform item in prefabsToInstantiate)
-
-        {
-            GameObject instantiatedPrefab = Instantiate(item.prefab, anchorObject.transform);
-            //Apply position offset relative to the camera's position
-            instantiatedPrefab.transform.localPosition = item.positionOffset;
-
-            // Apply rotation offset
-            instantiatedPrefab.transform.localRotation = Quaternion.Euler(item.rotationOffset);
-
-            // Apply scale
-            instantiatedPrefab.transform.localScale = item.scale;
-
-            instantiatedPrefab.transform.parent = anchorObject.transform;
-
-            instantiatedObjects.Add(instantiatedPrefab);
-
-            // Unparent, so that the instantiated objects will stay fixed to their location
+            GameObject instantiatedPrefab = Instantiate(prefab);
             //instantiatedPrefab.transform.SetParent(null, true);
 
-            //var instantiatedPrefab = Instantiate(item.prefab, item.positionOffset, Quaternion.Euler(item.rotationOffset));
-            //instantiatedPrefab.transform.localScale = item.scale;
-
-            //if (instantiatedPrefab.GetComponent<ARAnchor>() == null)
-            //{
-            //instantiatedPrefab.AddComponent<ARAnchor>();
-
-            //}
-
-            // GameObject instantiatedPrefab = Instantiate(item.prefab, trackedImage.transform);
-            // GameObject instantiatedPrefab = Instantiate(item.prefab, arCamera.transform); // Instantiates a prefab and sets its parent to the AR camera's transform.
-
-            // Apply position offset relative to the camera's position
-            //instantiatedPrefab.transform.localPosition = item.positionOffset;
-
-            // Apply rotation offset
-            //instantiatedPrefab.transform.localRotation = Quaternion.Euler(item.rotationOffset);
-
-            // Apply scale
-            //instantiatedPrefab.transform.localScale = item.scale;
-
-            // Unparent, so that the instantiated objects will stay fixed to their location
-            //instantiatedPrefab.transform.SetParent(null, true);
-
-            // Debugging logs
-            Debug.Log($"Instantiated Prefab: {item.prefab.name}");
-            Debug.Log($"Position Offset: {item.positionOffset}");
-            Debug.Log($"Rotation Offset: {item.rotationOffset}");
-            Debug.Log($"Scale: {item.scale}");
-
+            instantiatedPrefabs[trackedImage.referenceImage.name] = instantiatedPrefab;
         }
     }
 }
