258289742fc2972dd6491fc77e9f41ba45234956
CL Lam <corlisslam@gmail.com>
2024-07-26 21:05:15 -0700

    Used Git LFS for large files, altered narrativdesigntrigger script so that character starts speech when it is spawned

--- Assets/Convai/Scripts/Runtime/Features/NarrativeDesign/Runtime/NarrativeDesignTrigger.cs
+++ Assets/Convai/Scripts/Runtime/Features/NarrativeDesign/Runtime/NarrativeDesignTrigger.cs
@@ -1,3 +1,143 @@
+//using System.Collections.Generic;
+//using System.Linq;
+//using Convai.Scripts.Utils;
+//using UnityEngine;
+//using UnityEngine.Events;
+
+//namespace Convai.Scripts.Narrative_Design
+//{
+//    public class NarrativeDesignTrigger : MonoBehaviour
+//    {
+//        public ConvaiNPC convaiNPC;
+//        [HideInInspector] public int selectedTriggerIndex;
+//        [HideInInspector] public List<string> availableTriggers;
+//        public UnityEvent onTriggerEvent;
+
+//        private void Awake()
+//        {
+//            onTriggerEvent.AddListener(InvokeSelectedTrigger);
+//        }
+
+//        private void OnTriggerEnter(Collider other)
+//        {
+//            if (other.CompareTag("Player")) InvokeSelectedTrigger();
+//        }
+
+
+//        private void OnValidate()
+//        {
+//            availableTriggers = null;
+
+//            if (convaiNPC != null)
+//            {
+//                NarrativeDesignManager narrativeDesignManager = convaiNPC.GetComponent<NarrativeDesignManager>();
+//                if (narrativeDesignManager != null) availableTriggers = narrativeDesignManager.triggerDataList.Select(trigger => trigger.triggerName).ToList();
+//            }
+//        }
+
+//        /// <summary>
+//        ///   Invokes the selected trigger.
+//        /// </summary>
+//        public void InvokeSelectedTrigger()
+//        {
+//            if (convaiNPC != null && availableTriggers != null && selectedTriggerIndex >= 0 && selectedTriggerIndex < availableTriggers.Count)
+//            {
+//                string selectedTriggerName = availableTriggers[selectedTriggerIndex];
+//                ConvaiNPCManager.Instance.SetActiveConvaiNPC(convaiNPC);
+//                convaiNPC.TriggerEvent(selectedTriggerName);
+//            }
+//        }
+//    }
+//}
+//using System.Collections.Generic;
+//using System.Linq;
+//using Convai.Scripts.Utils;
+//using UnityEngine;
+//using UnityEngine.Events;
+
+//namespace Convai.Scripts.Narrative_Design
+//{
+//    public class NarrativeDesignTrigger : MonoBehaviour
+//    {
+//        public ConvaiNPC convaiNPC;
+//        [HideInInspector] public int selectedTriggerIndex;
+//        [HideInInspector] public List<string> availableTriggers;
+//        public UnityEvent onTriggerEvent;
+//        private CharacterSpawner1 characterSpawner;
+
+//        private void Awake()
+//        {
+//            //onTriggerEvent.AddListener(InvokeSelectedTrigger);
+//            //Debug.Log("Calling InvokeSelectedTrigger");
+//        }
+
+//        private void OnEnable()
+//        {
+//            //Following is code added on top of demo
+//            characterSpawner = FindObjectOfType<CharacterSpawner1>();
+//            if (characterSpawner != null)
+//            {
+//                // Subscribe to the OnCharacterSpawned event
+//                characterSpawner.OnCharacterSpawned += HandleCharacterSpawned;
+//                Debug.Log("Subscribed to OnCharacterSpawned event.");
+//            }
+//            else
+//            {
+//                Debug.LogWarning("CharacterSpawner1 instance not found in the scene.");
+//            }
+//            //End code added on top of demo
+//        }
+
+//        //private void Update()
+//        //{
+//        //    if (Input.GetKeyDown(KeyCode.B))
+//        //    {
+//        //        InvokeSelectedTrigger();
+//        //    }
+//        //}
+
+//        //Following is code added on top of demo
+//        private void HandleCharacterSpawned()
+//        {
+//            Debug.Log("Character spawned event received. Invoking selected trigger.");
+//            InvokeSelectedTrigger();
+//        }
+//        //End code added on top of demo
+
+//        private void OnTriggerEnter(Collider other)
+//        {
+//            if (other.CompareTag("Player")) InvokeSelectedTrigger();
+//        }
+
+
+//        private void OnValidate()
+//        {
+//            availableTriggers = null;
+
+//            if (convaiNPC != null)
+//            {
+//                NarrativeDesignManager narrativeDesignManager = convaiNPC.GetComponent<NarrativeDesignManager>();
+//                if (narrativeDesignManager != null) availableTriggers = narrativeDesignManager.triggerDataList.Select(trigger => trigger.triggerName).ToList();
+//            }
+//        }
+
+//        /// <summary>
+//        ///   Invokes the selected trigger.
+//        /// </summary>
+//        public void InvokeSelectedTrigger()
+//        {
+//            if (convaiNPC != null && availableTriggers != null && selectedTriggerIndex >= 0 && selectedTriggerIndex < availableTriggers.Count)
+//            {
+//                string selectedTriggerName = availableTriggers[selectedTriggerIndex];
+//                ConvaiNPCManager.Instance.SetActiveConvaiNPC(convaiNPC);
+//                convaiNPC.TriggerEvent(selectedTriggerName);
+//                Debug.Log("Trigger event invoked: " + selectedTriggerName);
+//            }
+//        }
+//    }
+//}
+
+//working on this currently
 using System.Collections.Generic;
 using System.Linq;
 using Convai.Scripts.Utils;
@@ -12,11 +152,52 @@ namespace Convai.Scripts.Narrative_Design
         [HideInInspector] public int selectedTriggerIndex;
         [HideInInspector] public List<string> availableTriggers;
         public UnityEvent onTriggerEvent;
+        //private CharacterSpawner1 characterSpawner;
 
         private void Awake()
         {
-            onTriggerEvent.AddListener(InvokeSelectedTrigger);
+            //onTriggerEvent.AddListener(InvokeSelectedTrigger);
+            //characterSpawner = FindObjectOfType<CharacterSpawner1>();
+
         }
+
+        private void Start()
+        {
+            InvokeSelectedTrigger();
+        }
+
+        //private void OnEnable()
+        //{
+        //    //Following is code added on top of demo
+        //    if (characterSpawner != null)
+        //    {
+        //        // Subscribe to the OnCharacterSpawned event
+        //        characterSpawner.OnCharacterSpawned += HandleCharacterSpawned;
+        //        Debug.Log("Subscribed to OnCharacterSpawned");
+        //    }
+        //    else
+        //    {
+        //        Debug.LogWarning("CharacterSpawner1 instance not found in the scene.");
+        //    }
+        //    //End code added on top of demo
+        //}
+
+        //private void Update()
+        //{
+        //    //if (Input.GetKeyDown(KeyCode.B))
+        //    //{
+        //    //    InvokeSelectedTrigger();
+        //    //}
+            
+        //}
+
+        ////Following is code added on top of demo
+        //private void HandleCharacterSpawned()
+        //{
+        //    Debug.Log("Calling InvokeSelectedTrigger");
+        //    InvokeSelectedTrigger();
+        //}
+        ////End code added on top of demo
 
         private void OnTriggerEnter(Collider other)
         {
@@ -45,7 +226,10 @@ namespace Convai.Scripts.Narrative_Design
                 string selectedTriggerName = availableTriggers[selectedTriggerIndex];
                 ConvaiNPCManager.Instance.SetActiveConvaiNPC(convaiNPC);
                 convaiNPC.TriggerEvent(selectedTriggerName);
+                Debug.Log("Called trigger");
             }
         }
     }
 }
+
+
--- Assets/Scripts/CharacterSpawner1.cs
+++ Assets/Scripts/CharacterSpawner1.cs
@@ -1,3 +1,142 @@
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.InputSystem.EnhancedTouch;
+//using UnityEngine.XR.ARFoundation;
+//using UnityEngine.XR.ARSubsystems;
+
+//public class CharacterSpawner1 : MonoBehaviour
+//{
+//    // Prefab for the Convai character
+//    public GameObject _characterPrefab;
+
+//    private ARTrackedImageManager _arTrackedImageManager;
+
+//    // Event triggered when a character is spawned
+//    public Action OnCharacterSpawned;
+
+//    /// <summary>
+//    /// Initializes necessary components and finds AR-related managers.
+//    /// </summary>
+//    ///
+
+
+//    private void Awake()
+//    {
+//        // Assuming this script is attached to the XR Origin GameObject
+//        _arTrackedImageManager = GetComponent<ARTrackedImageManager>();
+//    }
+
+//    private void OnEnable()
+//    {
+//        _arTrackedImageManager.trackedImagesChanged += ARTrackedImageManager_TrackedImagesChanged;
+//    }
+
+//    private void OnDisable()
+//    {
+//        _arTrackedImageManager.trackedImagesChanged -= ARTrackedImageManager_TrackedImagesChanged;
+//    }
+
+//    /// <summary>
+//    /// Handles tracked image changes, spawning characters on added images.
+//    /// </summary>
+//    /// <param name="eventArgs">Event arguments containing information about tracked image changes.</param>
+//    private void ARTrackedImageManager_TrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+//    {
+//        foreach (var trackedImage in eventArgs.added)
+//        {
+//            // Handle Added Event
+//            Debug.Log("Tracked image added: " + trackedImage.referenceImage.name);
+//            GameObject character = SpawnCharacter();
+//            character.transform.parent = trackedImage.transform; ;
+//        }
+//    }
+
+//    private GameObject SpawnCharacter()
+//    {
+//        GameObject character = Instantiate(_characterPrefab);
+
+//        OnCharacterSpawned?.Invoke();
+
+//        return character;
+//    }
+
+//}
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.InputSystem.EnhancedTouch;
+//using UnityEngine.XR.ARFoundation;
+//using UnityEngine.XR.ARSubsystems;
+
+//public class CharacterSpawner1 : MonoBehaviour
+//{
+//    // Prefab for the Convai character
+//    public GameObject _characterPrefab;
+
+//    private ARTrackedImageManager _arTrackedImageManager;
+
+//    // Event triggered when a character is spawned
+//    public Action OnCharacterSpawned;
+
+//    /// <summary>
+//    /// Initializes necessary components and finds AR-related managers.
+//    /// </summary>
+//    private void Awake()
+//    {
+//        // Assuming this script is attached to the XR Origin GameObject
+//        _arTrackedImageManager = GetComponent<ARTrackedImageManager>();
+//    }
+
+//    private void OnEnable()
+//    {
+//        _arTrackedImageManager.trackedImagesChanged += ARTrackedImageManager_TrackedImagesChanged;
+//    }
+
+//    private void OnDisable()
+//    {
+//        _arTrackedImageManager.trackedImagesChanged -= ARTrackedImageManager_TrackedImagesChanged;
+//    }
+
+//    /// <summary>
+//    /// Handles tracked image changes, spawning characters on added images.
+//    /// </summary>
+//    /// <param name="eventArgs">Event arguments containing information about tracked image changes.</param>
+//    private void ARTrackedImageManager_TrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+//    {
+//        foreach (var trackedImage in eventArgs.added)
+//        {
+//            // Handle Added Event
+//            Debug.Log("Tracked image added: " + trackedImage.referenceImage.name);
+//            Debug.Log("Calling SpawnCharacter()");
+//            GameObject character = SpawnCharacter();
+//            //SpawnCharacter();
+//            //Debug.Log("Character spawned: " + character.name);
+//            //character.transform.SetParent(null, true);
+//            //Debug.Log("Character parent set as null");
+
+//        }
+//    }
+
+//    private GameObject SpawnCharacter()
+//    {
+//        Debug.Log("Spawning character");
+//        GameObject character = Instantiate(_characterPrefab);
+//        Debug.Log("Character spawned");
+
+//        OnCharacterSpawned?.Invoke();
+//        Debug.Log("Invoke subscription to OnCharacterSpwaned");
+
+//        //Debug.Log("Returning character")
+//        return character;
+//    }
+
+//}
+
+
+// working on this version rn
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,13 +185,16 @@ public class CharacterSpawner1 : MonoBehaviour
             // Handle Added Event
             Debug.Log("Tracked image added: " + trackedImage.referenceImage.name);
             GameObject character = SpawnCharacter();
-            character.transform.SetParent(null, true);
+            character.transform.parent = trackedImage.transform;
+            
+            //character.transform.SetParent(null, true);
         }
     }
 
     private GameObject SpawnCharacter()
     {
         GameObject character = Instantiate(_characterPrefab);
+        Debug.Log("Instantiated character: " + character.name);
 
         OnCharacterSpawned?.Invoke();
 
